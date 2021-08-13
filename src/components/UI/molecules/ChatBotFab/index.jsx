01cdb9d826baf27697f60f410fcd5d88b3f4262d
armcruz <jaimes087.uz@gmail.com>
2021-08-13 00:39:06 -0500

    style: combine badge with chatbotfab

--- src/components/UI/molecules/ChatBotFab/index.jsx
+++ src/components/UI/molecules/ChatBotFab/index.jsx
@@ -2,18 +2,20 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { MdChatBubble } from 'react-icons/md';
 import styles from './ChatBotFab.module.css';
+import Badge from '../../atoms/Badge';
 
-function ChatBotFAB({ hint = 'Click to open chat with Chatty Assistant' }) {
+function ChatBotFab({ hint = 'Click to open chat with Chatty Assistant' }) {
 	return ReactDOM.createPortal(
 		<button aria-label={hint} className={styles.chatbotFab} type="button">
 			<MdChatBubble size="28" color="currentColor" />
+			<Badge badgeContent={1} />
 		</button>,
 		window.document.getElementById('chatbot-fab-root')
 	);
 }
 
-ChatBotFAB.propTypes = {
+ChatBotFab.propTypes = {
 	hint: PropTypes.string,
 };
 
-export default ChatBotFAB;
+export default ChatBotFab;
