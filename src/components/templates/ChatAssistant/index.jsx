7b27d1ddadeb050c1f498e906cd2ed957f43dbe9
armcruz <jaimes087.uz@gmail.com>
2021-08-13 15:51:05 -0500

    other: add the complete chat assistant widget

---
+++ src/components/templates/ChatAssistant/index.jsx
@@ -0,0 +1,13 @@
+import Chat from '../Chat';
+import ChatBotFab from 'components/UI/molecules/ChatBotFab';
+
+function ChatAsistant() {
+	return (
+		<div>
+			<Chat />
+			<ChatBotFab />
+		</div>
+	);
+}
+
+export default ChatAsistant;
---
+++ src/components/templates/ChatAssistant/index.stories.jsx
@@ -0,0 +1,8 @@
+import ChatAssistantComponent from '.';
+
+export default {
+	title: 'Templates/ChatAssistant',
+	component: ChatAssistantComponent,
+};
+
+export const ChatAssistant = () => <ChatAssistantComponent />;
