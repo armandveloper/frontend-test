618c68b38fc1275f7b019cf63f54f26408307ea2
armcruz <jaimes087.uz@gmail.com>
2021-08-13 22:37:45 -0500

    feat: add emoji picker

--- src/components/UI/organisms/ChatBottom/index.jsx
+++ src/components/UI/organisms/ChatBottom/index.jsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import { MdFace, MdSend } from 'react-icons/md';
+import Picker from 'emoji-picker-react';
 import styles from './ChatBottom.module.css';
 import Textarea from 'components/UI/atoms/Textarea';
 import { useChatContext } from 'context/ChatContext';
 
 function ChatBottom() {
 	const [message, setMessage] = React.useState('');
+
+	const [showEmojiPicker, setShowEmojiPicker] = React.useState(false);
+
+	const toggleEmojiPicker = () => setShowEmojiPicker(!showEmojiPicker);
+
+	const handleEmojiClick = (_, emojiObject) => {
+		setMessage((message) => `${message}${emojiObject.emoji}`);
+		setShowEmojiPicker(false);
+	};
 
 	const { sendMessage } = useChatContext();
 
@@ -30,11 +40,24 @@ function ChatBottom() {
 					placeholder="Reply to Chatty Assistant"
 				/>
 			</div>
+
+			{showEmojiPicker && (
+				<Picker
+					onEmojiClick={handleEmojiClick}
+					pickerStyle={{
+						position: 'absolute',
+						bottom: 60,
+						right: 0,
+					}}
+				/>
+			)}
+
 			<button
 				aria-label="Click to open emoji picker"
 				className={styles.btn}
 				title="Click to open emoji picker"
 				type="button"
+				onClick={toggleEmojiPicker}
 			>
 				<MdFace color="gray" size="24" />
 			</button>
